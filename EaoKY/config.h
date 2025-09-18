d37482a4001c2b6cf100d217f49c62fdf6980f07
Teije van Hemert <teijevhemert@gmail.com>
2025-09-18 15:58:56 +0200

    Add cirque trackpad support with an automatic mouse layer

--- EaoKY/config.h
+++ EaoKY/config.h
@@ -8,3 +8,9 @@
 
 #define RGB_MATRIX_STARTUP_SPD 60
 
+#define CIRQUE_PINNACLE_TAP_ENABLE             // Enables tap-to-click functionality on the Cirque Pinnacle touchpad, allowing single taps to act as left-clicks
+#define POINTING_DEVICE_ROTATION_90            // Rotates the X and Y data from the pointing device by 90 degrees, useful if the device is physically oriented differently
+#define MOUSE_EXTENDED_REPORT                  // Enables support for extended mouse reports, increasing the range of movement values to -32767 to 32767
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE      // Enables the automatic mouse layer feature to activate a specific layer when the pointing device is in use
+#define AUTO_MOUSE_TIME 280                    // Sets the amount of time (in ms) that the mouse layer remains active after activation; ideal values are 250-1000 ms
+#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE // Enables scroll gestures on compatible devices, allowing actions like circular or side scrolling
--- EaoKY/rules.mk
+++ EaoKY/rules.mk
@@ -4,3 +4,6 @@ ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
+
+POINTING_DEVICE_ENABLE = yes                 # Enables the pointing device feature in QMK firmware, allowing the use of mouse-like inputs
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c # Specifies the driver to be used for the pointing device, e.g., Cirque Pinnacle touchpad
